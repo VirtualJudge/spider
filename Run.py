64602c00e7b94d64e9d3391a94ad9a4faa6d5696
LittleSepia <LasNoire23@gmail.com>
2018-03-10 22:37:59 +0800

    更新Run.py

--- Run.py
+++ Run.py
@@ -2,11 +2,13 @@ from Control import Controller
 from Config import Account
 
 if __name__ == '__main__':
-    account = Account('', '')
+    account = Account('genesis244', 'herui130130')
     # Controller.get_problem('HDU', '2011').show()
-    # Controller.check_status('HDU')
-    # Controller.find_language('HDU', account)
+    #Controller.check_status('POJ')
+    #print(Controller.find_language('POJ', account))
     # Controller.is_waiting_for_judge()
     # Controller.get_result_by_rid()
     # Controller.get_result()
     # Controller.submit_code()
+    #with open('/home/noire/SpiderTest/1000.cpp','r') as fin:
+       # Controller.submit_code('HDU',account,fin.read(),'0',1000)
