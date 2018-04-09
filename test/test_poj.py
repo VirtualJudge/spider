ebc8f01d78636a0d90c4fbf60be09717c907b067
prefixai <xudian.cn@gmail.com>
2018-04-09 17:55:04 +0800

    update unittest

--- test/test_poj.py
+++ test/test_poj.py
@@ -1,10 +1,10 @@
 import unittest
 
-from VirtualJudgeSpider.Control import Controller
 from VirtualJudgeSpider.Config import Account
-import os
-import json
+from VirtualJudgeSpider.Control import Controller
+
 
 class TestPOJ(unittest.TestCase):
     def test_get_problem(self):
-        Controller('POJ').get_problem('1000',)
+        account = Account('robot4test', 'robot4test')
+        Controller('POJ').get_problem('1000', account)
