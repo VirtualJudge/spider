0a3ae2f6ef664ae9fcc3a52954c0757274797c86
prefixai <xudian.cn@gmail.com>
2018-04-09 21:17:39 +0800

    update server test

--- server-test.py
+++ server-test.py
@@ -4,47 +4,25 @@ from urllib.parse import urlparse
 from VirtualJudgeSpider.Control import Controller
 from VirtualJudgeSpider.Config import Account
 
+from flask import Flask, render_template, Blueprint
+from VirtualJudgeSpider.Control import Controller
+from VirtualJudgeSpider.Config import Account
 
-class MyHTTPRequestHandler(BaseHTTPRequestHandler):
-    # GET
-    def do_GET(self):
-        querypath = urlparse(self.path)
-        if querypath.path == '/':
-            with open('server.html', 'rb') as f:
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(f.read())
-        else:
-            param_list = str(querypath.path).split('/')
-            param_list.remove('')
-            if len(param_list) == 2:
-                self.send_response(200)
-                self.end_headers()
-                remote_oj = param_list[0]
-                remote_id = param_list[1]
-                account = Account('robot4test', 'robot4test')
-                problem = Controller(remote_oj).get_problem(remote_id, account)
-                if problem:
-                    self.wfile.write(bytes(problem.html, encoding='utf-8'))
-                else:
-                    self.send_response(404)
-            else:
-                self.send_response(404)
+app = Flask(__name__, template_folder='.')
 
 
-def run():
-    port = 7777
-    print('starting server, port', port)
+@app.route("/<string:remote_oj>/<string:remote_id>")
+def problem(remote_oj, remote_id):
+    problem = Controller(remote_oj).get_problem(remote_id, account=Account('robot4test', 'robot4test'))
+    if problem:
+        return problem.html
+    return None
 
-    # Server settings
-    server_address = ('0.0.0.0', port)
-    httpd = HTTPServer(server_address, MyHTTPRequestHandler)
-    print('running server: http://localhost:8000 ...')
-    httpd.serve_forever()
+
+@app.route("/")
+def index():
+    return render_template('server.html')
 
 
 if __name__ == '__main__':
-    try:
-        run()
-    except:
-        print('bye bye')
+    app.run(debug=False, port=7777)
