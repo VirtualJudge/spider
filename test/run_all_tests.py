82343b000a2bb8f282b704954009db55055529c7
prefixai <xudian.cn@gmail.com>
2018-04-10 16:30:27 +0800

    fix unit test

--- test/run_all_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import os
-
-if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    runner.run(unittest.defaultTestLoader.discover(start_dir=os.path.dirname(os.path.abspath(__file__)),
-                                                   pattern="test_*.py"))
