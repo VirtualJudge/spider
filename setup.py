2833024766d7b828a5b88fe0daccdf8b4e556bc0
prefixai <xudian.cn@gmail.com>
2018-04-10 20:36:44 +0800

    update version info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='VirtualJudgeSpider',
-      version='0.7.0',
+      version='0.7.1',
       description='Virtual Judge Spider',
       author='dian xu',
       author_email='xudian.cn@gmail.com',
