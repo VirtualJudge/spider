5852ae1ff4a24252ab2c2649e38549a4d677d8bb
prefixai <xudian.cn@gmail.com>
2018-04-14 23:29:48 +0800

    fix network error

--- VirtualJudgeSpider/Utils.py
+++ VirtualJudgeSpider/Utils.py
@@ -10,14 +10,14 @@ class HttpUtil(object):
         self._headers = custom_headers
         self._request = requests.session()
         self._code_type = code_type
-
+        self._timeout = (3.03, 6)
         self._response = None
         if self._headers:
             self._request.headers.update(self._headers)
 
     def get(self, url, **kwargs):
         try:
-            self._response = self._request.get(url, **kwargs)
+            self._response = self._request.get(url, timeout=self._timeout, **kwargs)
             if self._code_type and self._response:
                 self._response.encoding = self._code_type
             return self._response
@@ -26,7 +26,7 @@ class HttpUtil(object):
 
     def post(self, url, data=None, json=None, **kwargs):
         try:
-            self._response = self._request.post(url, data, json, **kwargs)
+            self._response = self._request.post(url, data, json, timeout=self._timeout, **kwargs)
             if self._code_type and self._response:
                 self._response.encoding = self._code_type
             return self._response
