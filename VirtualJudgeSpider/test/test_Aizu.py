e582319394bf10d8229a2b6aded2c609117c19e7
mr-kkid <liwenshi1996@gmail.com>
2018-04-16 22:34:32 +0800

    add WUST/Aizu/FZU/ZOJ test file

---
+++ VirtualJudgeSpider/test/test_Aizu.py
@@ -0,0 +1,11 @@
+import unittest
+
+from VirtualJudgeSpider.Config import Account
+from VirtualJudgeSpider.Control import Controller
+
+
+class TestPOJ(unittest.TestCase):
+    def test_get_problem(self):
+        account = Account('robot4test', 'robot4test')
+        Controller('Aizu').get_problem('1000', account)
+        Controller('Aizu').get_result(account=account, pid='1000')
---
+++ VirtualJudgeSpider/test/test_FZU.py
@@ -0,0 +1,11 @@
+import unittest
+
+from VirtualJudgeSpider.Config import Account
+from VirtualJudgeSpider.Control import Controller
+
+
+class TestPOJ(unittest.TestCase):
+    def test_get_problem(self):
+        account = Account('robot4test', 'robot4test')
+        Controller('FZU').get_problem('1000', account)
+        Controller('FZU').get_result(account=account, pid='1000')
