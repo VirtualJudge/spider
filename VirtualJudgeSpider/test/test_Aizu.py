8097e484436f1a2fb41581cb6690c75085f778da
mr-kkid <36004457+mr-kkid@users.noreply.github.com>
2018-04-16 23:13:35 +0800

    Delete test_Aizu.py

--- VirtualJudgeSpider/test/test_Aizu.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from VirtualJudgeSpider.Config import Account
-from VirtualJudgeSpider.Control import Controller
-
-
-class TestAizu(unittest.TestCase):
-    def test_get_problem(self):
-		pass
