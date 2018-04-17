14bc139255e93103f5d9cc44448f93a59ba7dd0b
mr-kkid <liwenshi1996@gmail.com>
2018-04-17 22:57:58 +0800

    add test_aizu

---
+++ VirtualJudgeSpider/test/test_aizu.py
@@ -0,0 +1,11 @@
+import unittest
+
+from VirtualJudgeSpider.Config import Account
+from VirtualJudgeSpider.Control import Controller
+
+
+class TestAizu(unittest.TestCase):
+    def test_get_problem(self):
+        account = Account('robot4test', 'robot4test')
+        Controller('Aizu').get_problem('0001', account)
+        Controller('Aizu').get_result(account=account, pid='0001')
