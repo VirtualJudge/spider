f20d03d7ec58ebf6c6e38e293788addce8302a2c
mr-kkid <liwenshi1996@gmail.com>
2018-04-17 23:10:12 +0800

    fix test_aizu.py

--- VirtualJudgeSpider/test/test_aizu.py
+++ VirtualJudgeSpider/test/test_aizu.py
@@ -8,4 +8,4 @@ class TestAizu(unittest.TestCase):
     def test_get_problem(self):
         account = Account('robot4test', 'robot4test')
         Controller('Aizu').get_problem('0001', account)
-        Controller('Aizu').get_result(account=account, pid='0001')
+        
