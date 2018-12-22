f8934bf9e0ee35679d10bd8097375654f2581bfb
prefixai <xudian.cn@gmail.com>
2018-12-22 20:06:39 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup
+import setuptools
 
-setup(name='VirtualJudgeSpider',
-      version='0.8.0',
-      description='Virtual Judge Spider',
-      author='dian xu',
-      author_email='xudian.cn@gmail.com',
-      packages=['VirtualJudgeSpider', 'VirtualJudgeSpider/OJs'],
-      install_requires=['beautifulsoup4', 'lxml', 'requests']
-      )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='VirtualJudgeSpider',
+                 version='1.0',
+                 description='Virtual Judge Spider',
+                 author='prefixai',
+                 author_email='xudian.cn@gmail.com',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(),
+                 install_requires=['beautifulsoup4', 'lxml', 'requests'],
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ]
+                 )
